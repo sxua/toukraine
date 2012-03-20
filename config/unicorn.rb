42977c09bcb7a7fe4819a8a81aa749fc2bceadfb
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-03-20 22:16:38 +0200

    unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,8 @@ working_directory "/var/www/toukraine"
 preload_app true
 listen "/tmp/.toukraine_socket", :backlog => 64
 pid "/var/www/toukraine/tmp/pids/unicorn.pid"
+stderr_path "/var/www/toukraine/log/unicorn.stderr.log"
+stdout_path "/var/www/toukraine/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
