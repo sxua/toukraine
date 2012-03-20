649a4f7488188b756db80097c458554a5dafcbab
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-03-20 21:45:50 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toukraine::Application.config.secret_token = '848a20c3cafbfd5fc75c72def51558a1603c7efa08f1dbcbcff2aef607c48ad1bf18056ff36b1949b8342533ae8c3bf9e80440970497ad473da423b5c99a4ce0'
