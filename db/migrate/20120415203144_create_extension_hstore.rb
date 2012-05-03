2de75d95a0bf353d5ba9518b0d26c3ec4cde85c7
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-05-03 03:21:36 +0300

    test

--- db/migrate/20120415203144_create_extension_hstore.rb
+++ db/migrate/20120415203144_create_extension_hstore.rb
@@ -1,9 +1,9 @@
 class CreateExtensionHstore < ActiveRecord::Migration
   def up
-    execute("CREATE EXTENSION hstore")
+    execute("CREATE EXTENSION hstore") unless Rails.env.production?
   end
 
   def down
-    execute("DROP EXTENSION hstore")
+    execute("DROP EXTENSION hstore") unless Rails.env.production?
   end
 end
