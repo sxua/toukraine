9accba7f5daca6d961909d69465ef932af614440
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-05-14 21:47:34 +0300

    pages

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -1,3 +1,39 @@
 ActiveAdmin.register Page do
+  form do |f|
+    f.inputs "Page" do
+      f.input :title_ru
+      f.input :body_ru
+      f.input :slug_ru
+      f.input :title_en
+      f.input :body_en
+      f.input :slug_en
+    end
+    f.inputs "Publish" do
+      f.input :is_published
+    end
+    f.buttons
+  end
   
+  controller do
+    def show
+      @page = Page.fetch(params[:id]) || Page.new
+    end
+
+    def edit
+      @page = Page.fetch(params[:id]) || Page.new
+    end
+    
+    def update
+      @page = Page.fetch(params[:id])
+      super
+    end
+  end
+  
+  before_create do |page|
+    page.set_creator current_admin_user
+  end
+  
+  before_save do |page|
+    page.set_publisher current_admin_user
+  end
 end
