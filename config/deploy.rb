223e2eef3833ddf691508457d5958a0630b83b5f
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-05-15 21:30:31 +0300

    use capistrno shared_children

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,8 @@ set :ssh_options, { forward_agent: true }
 set :bundle_flags, "--deployment --quiet --binstubs --shebang ruby-local-exec"
 set :default_environment, { 'PATH' => "/opt/rbenv/shims:/opt/rbenv/bin:$PATH" }
 
+set :shared_children, shared_children + %w(public/uploads)
+
 set :unicorn_config, "#{current_path}/config/unicorn.rb"
 set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 
@@ -108,40 +110,4 @@ namespace :unicorn do
   task :less, except: { no_release: true } do
     run "#{try_sudo} kill -s TTOU `cat #{unicorn_pid}`"
   end
-end
-
-# ==============================
-# Uploads
-# ==============================
-
-namespace :uploads do
-  desc <<-EOD
-    Creates the upload folders unless they exist
-    and sets the proper upload permissions.
-  EOD
-  task :setup, except: { no_release: true } do
-    dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
-    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
-  end
-
-  desc <<-EOD
-    [internal] Creates the symlink to uploads shared folder
-    for the most recently deployed version.
-  EOD
-  task :symlink, except: { no_release: true } do
-    run "rm -rf #{release_path}/public/uploads"
-    run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
-  end
-
-  desc <<-EOD
-    [internal] Computes uploads directory paths
-    and registers them in Capistrano environment.
-  EOD
-  task :register_dirs do
-    set :uploads_dirs, %w(uploads)
-    set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
-  end
-
-  after "deploy:finalize_update", "uploads:symlink"
-  on :start, "uploads:register_dirs"
 end
