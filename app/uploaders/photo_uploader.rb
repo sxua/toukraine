123529096f3a3ec44ab49039c981f2528e0e62c2
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-05-17 17:24:06 +0300

    use public/system instead of public/uploads

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -7,7 +7,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "system/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
