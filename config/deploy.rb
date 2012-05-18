17afc43a55d22b528239a946017df65f7b14bf71
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-05-18 12:48:31 +0300

    precompile assets only when needed

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,19 @@ def remote_process_exists?(pid_file)
   capture("ps -p $(cat #{pid_file}) ; true").strip.split("\n").size == 2
 end
 
+namespace :deploy do
+  namespace :assets do
+    task :precompile, roles: :web, except: { no_release: true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes."
+      end
+    end
+  end
+end
+
 namespace :db do
   desc "Backup the database"
   task :backup, roles: :db do
