bafa50f7032a3f9318a9717d55472b68b78fbf02
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-05-23 15:26:08 +0300

    capistrano db backup

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,19 +46,32 @@ namespace :deploy do
 end
 
 namespace :db do
-  desc "Backup the database"
-  task :backup, roles: :db do
-    run "mkdir -p #{current_path}/backups"
-    run "cd #{current_path}; pg_dump -U postgres #{application}_production -f backups/#{Time.now.utc.strftime('%Y%m%d%H%M%S')}.sql"
+  task :backup_name, roles: :db, only: { primary: true } do
+    now = Time.now
+    run "mkdir -p #{shared_path}/backups"
+    backup_time = [now.year, now.month, now.day, now.hour, now.min, now.sec].join('-')
+    set :backup_file, "#{shared_path}/backups/#{application}-snapshot-#{backup_time}.sql"
   end
-
-  desc "Backup the database and download the script"
-  task :download, roles: :app do
-    db
-    timestamp = Time.now.utc.strftime('%Y%m%d%H%M%S') 
-    run "mkdir -p backups"
-    run "cd #{current_path}; tar -cvzpf #{timestamp}_backup.tar.gz backups"
-    get "#{current_path}/#{timestamp}_backup.tar.gz", "#{timestamp}_backup.tar.gz"
+  
+  task :dump, roles: :db, only: { primary: true } do
+    backup_name
+    run("cat #{shared_path}/config/database.yml") { |channel, stream, data| @environment_info = YAML.load(data)[rails_env] }
+    dbuser = @environment_info['username']
+    dbpass = @environment_info['password']
+    environment_database = @environment_info['database']
+    dbhost = @environment_info['host']
+    run "pg_dump -W -c -U #{dbuser} -h #{dbhost} #{environment_database} | bzip2 -c > #{backup_file}.bz2" do |channel, stream, out|
+      channel.send_data "#{dbpass}\n" if out =~ /^Password:/
+    end
+  end
+  
+  task :clone_local, roles: :db, only: { primary: true } do
+    backup_name
+    dump
+    get "#{backup_file}.bz2", "/tmp/#{application}.sql.bz2"
+    development_info = YAML.load_file("config/database.yml")['development']
+    run_str = "PGPASSWORD=#{development_info['password']} bzcat /tmp/#{application}.sql.bz2 | psql -U #{development_info['username']} -h #{development_info['host']} #{development_info['database']}"
+    %x!#{run_str}!
   end
 end
 
