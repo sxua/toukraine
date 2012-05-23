9d6af319e1dff66310eb30e0781f2acc522226bd
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-05-23 15:25:53 +0300

    old shit

--- db/fixtures/hotel_types.rb
+++
@@ -1,7 +0,0 @@
-HotelType.seed_once(:title_ru,
-  {id: 1, title_ru: 'гостиница', title_en: 'hotel'},
-  {id: 2, title_ru: 'отель', title_en: 'hotel'},
-  {id: 3, title_ru: 'санаторий', title_en: 'sanatorium'},
-  {id: 4, title_ru: 'ТОК', title_en: 'recreational hotel'},
-  {id: 5, title_ru: 'пансионат', title_en: 'pension'}
-)
