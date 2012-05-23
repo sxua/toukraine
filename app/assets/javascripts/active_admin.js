4a04d81dda967acc202166e7c5910e05d653e9ec
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-05-23 15:26:33 +0300

    backend

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,7 +1,9 @@
 //= require active_admin/base
 //= require bootstrap-tabs
 //= require ckeditor/init
+//= require chosen.jquery
 
 $(function(){
+  $('select').chosen();
   $('.tabbable a:first').tab('show');
 });
