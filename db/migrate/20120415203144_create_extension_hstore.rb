60f8f02cff830fba68b2f2afffd594afb86edd30
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-05-23 14:49:20 +0300

    drop hstore extension creation, use postgres templates instead

--- db/migrate/20120415203144_create_extension_hstore.rb
+++
@@ -1,9 +0,0 @@
-class CreateExtensionHstore < ActiveRecord::Migration
-  def up
-    execute("CREATE EXTENSION hstore") unless Rails.env.production?
-  end
-
-  def down
-    execute("DROP EXTENSION hstore") unless Rails.env.production?
-  end
-end
