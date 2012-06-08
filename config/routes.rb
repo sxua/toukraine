d89ed586156ffd95e28e2f381a8ec85553cb9ebd
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-06-08 03:37:29 +0300

    some routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,10 @@ Toukraine::Application.routes.draw do
     resources :promotions
     resources :pages
     resources :news
-    resources :regions do
-      get :hotels, on: :member
-    end
-    match '/hotels/:region_id/:id', to: 'hotels#show', as: :hotel_region
+    resources :regions
+    match '/regions/:id/hotels(/sort/:column::dir)(/page/:page)', to: 'regions#hotels', as: :region_hotels
     match '/hotels/:city_id/:id', to: 'hotels#show', as: :hotel_city
     match '/tours/:tour_type_id/:id', to: 'tours#show', as: :tour
-    match '/tours/:id', to: 'tour_types#show', as: :tour_type
+    match '/tours/:id(/sort/:column::dir)(/page/:page)', to: 'tour_types#show', as: :tour_type
   end
 end
