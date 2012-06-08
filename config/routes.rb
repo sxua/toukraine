2b2329f24587cdcb7d06c983a2b2630a4f477e16
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-06-08 04:06:03 +0300

    fix route name

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Toukraine::Application.routes.draw do
     resources :pages
     resources :news
     resources :regions
-    match '/regions/:id/hotels(/sort/:column::dir)(/page/:page)', to: 'regions#hotels', as: :region_hotels
+    match '/regions/:id/hotels(/sort/:column::dir)(/page/:page)', to: 'regions#hotels', as: :hotels_region
     match '/hotels/:city_id/:id', to: 'hotels#show', as: :hotel_city
     match '/tours/:tour_type_id/:id', to: 'tours#show', as: :tour
     match '/tours/:id(/sort/:column::dir)(/page/:page)', to: 'tour_types#show', as: :tour_type
