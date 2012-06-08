c9313570b79155c1ae6726a746f8cdc74aa3c14a
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-06-08 04:25:08 +0300

    include city in query

--- app/controllers/tour_types_controller.rb
+++ app/controllers/tour_types_controller.rb
@@ -1,7 +1,7 @@
 class TourTypesController < ApplicationController
   def show
     @tour_type = TourType.find(params[:id])
-    @tours = @tour_type.tours
+    @tours = @tour_type.tours.includes(:city)
     if params[:column] && sort_columns_for(Tour).has_key?(:"#{params[:column]}")
       @tours = @tours.order("#{sort_columns_for(Tour)[:"#{params[:column]}"]} #{sanitize_dir(params[:dir])} NULLS LAST")
     end
