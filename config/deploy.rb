74a7b314d1d7a13ff6239847360ed38902080b6a
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-06-08 04:24:53 +0300

    don't precompile assets everytime

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,19 +35,6 @@ def remote_process_exists?(pid_file)
 end
 
 namespace :deploy do
-  namespace :assets do
-    task :precompile, roles: :web, except: { no_release: true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes."
-      end
-    end
-  end
-end
-
-namespace :deploy do
   task :create_symlink do
     set :current_path, "#{deploy_to}/current"
     on_rollback do
@@ -59,5 +46,16 @@ namespace :deploy do
     end
     
     run "rm -f #{current_path} && ln -s #{latest_release} #{current_path}"
+  end
+
+  namespace :assets do
+    task :precompile, roles: :web, except: { no_release: true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes."
+      end
+    end
   end
 end
