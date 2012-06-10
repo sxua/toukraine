7255c1e3ddb66979dec4f8ee29bf9740a9fd5d71
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-06-10 17:59:21 +0300

    hello Options

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,12 +1,11 @@
-class Option; end
-# class Option < ActiveRecord::Base
-#   attr_accessible :key, :title, :value
-# 
-#   validates_presence_of :key, :value
-# 
-#   self.all.each do |option|
-#     define_singleton_method(option.key) do
-#       option.value
-#     end
-#   end
-# end
+class Option < ActiveRecord::Base
+  attr_accessible :key, :title, :value
+
+  validates_presence_of :key, :value
+
+  self.all.each do |option|
+    define_singleton_method(option.key) do
+      option.value
+    end
+  end
+end
