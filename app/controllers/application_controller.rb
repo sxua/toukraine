4648e84b2f7c849c894019f7fe78abded11a9288
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-06-27 04:53:09 +0300

    small fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
   protect_from_forgery
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
-  rescue_from ArgumentError, with: :not_found
-  
+  # rescue_from ArgumentError, with: :not_found
+
   def set_locale
     I18n.locale = params[:locale] || cookies[:locale] || get_user_preferred_locale || I18n.default_locale
     cookies[:locale] = I18n.locale
@@ -12,11 +12,11 @@ class ApplicationController < ActionController::Base
   def get_user_preferred_locale
     language = request.compatible_language_from(%w(ru en)) || 'ru'
   end
-  
+
   def default_url_options(options={})
     { locale: I18n.locale }
   end
-  
+
   def not_found
     render file: "#{Rails.root}/public/404.html", status: 404, layout: false
   end
