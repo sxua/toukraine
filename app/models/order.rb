781b5a6bbeeee6035bd95532707d1966a9dd7609
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-07-09 15:01:03 +0300

    try this

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ActiveRecord::Base
   belongs_to :relative, polymorphic: true
   attr_accessible :email, :name, :relative_type, :relative_id, :phone, :come_in_date, :come_out_date, :people_amount, :description
-  validates_presence_of :relative_id, :name, :email, :phone, :come_in_date, :come_out_date, :people_amount
+  validates_presence_of :relative_id, :name, :email, :phone, :come_in_date, :come_out_date
+  validates :people_amount, presence: true, numericality: { greater_than: 0 }
 end
