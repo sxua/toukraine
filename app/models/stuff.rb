3f00d03c23ad2f037c262414f3cc0e9d0316d8e2
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-08-15 12:10:02 +0300

    limit stuff to 10

--- app/models/stuff.rb
+++ app/models/stuff.rb
@@ -1,8 +1,8 @@
 class Stuff < ActiveRecord::Base
-  def self.shit
+  def self.articles(limit)
     stuff = []
     stuff += News.for_locale(I18n.locale).limit(5)
     stuff += Promotion.for_locale(I18n.locale)
-    stuff.sort_by(&:created_at).reverse
+    stuff.sort_by(&:created_at).reverse[0...limit]
   end
 end
