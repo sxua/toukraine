ab0fedef544696b41a89c415304b7ddf0cecdfed
Andrey Melnik <druid.bpg@gmail.com>
2012-09-19 17:13:35 +0300

    Enabled ActiveAdmin page to after migrations

--- app/admin/background.rb
+++ app/admin/background.rb
@@ -1,11 +1,11 @@
-# ActiveAdmin.register Background do
-#   index do
-#     column :id
-#     column :image do |bg|
-#       image_tag bg.image.url(:thumb)
-#     end
-#     default_actions
-#   end
-#   
-#   filter :id
-# end
+ActiveAdmin.register Background do
+  index do
+    column :id
+    column :image do |bg|
+      image_tag bg.image.url(:thumb)
+    end
+    default_actions
+  end
+  
+  filter :id
+end
