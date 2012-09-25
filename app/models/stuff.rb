3ad0f475b8ba13a95d0a6c104233138c9c930b94
Andrey Melnik <druid.bpg@gmail.com>
2012-09-25 17:23:16 +0300

    Aside new block datatime

--- app/models/stuff.rb
+++ app/models/stuff.rb
@@ -1,7 +1,7 @@
 class Stuff < ActiveRecord::Base
   def self.articles(limit = -1)
     stuff = []
-    stuff += News.for_locale(I18n.locale).limit(5)
+    stuff += News.for_locale(I18n.locale).order("created_at DESC").limit(5)
     stuff += Promotion.for_locale(I18n.locale)
     stuff.sort_by(&:created_at).reverse[0...limit]
   end
